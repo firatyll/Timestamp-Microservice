2c0abca3d4cf00f6affee3e9c346a853b1892923
Fırat Yolal <firatyll213@gmail.com>
2023-11-25 00:48:47 +0300

    cors added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express =require("express")
 const app = express()
 require("dotenv").config()
+const cors = require("cors")
 app.use(cors())
 
 const routes = require("./routes/timestamproutes")
